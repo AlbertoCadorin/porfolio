cef4d9e4233d51af84dd57e7b73be8cea60a0fbc
AlbertoCadorin <alberto.cadori01@gmail.com>
2026-02-04 14:12:16 +0100

    Add project data for portfolio showcase

---
+++ src/data/project.js
@@ -0,0 +1,39 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Booroad",
+        description: "Booroad è una web-app progettata per esplorare nuove destinazioni, creare itinerari personalizzati e pianificare il tuo viaggio in modo semplice e veloce. L’app è costruita utilizzando Vite, uno strumento di sviluppo di ultimissima generazione pensato per progetti frontend moderni.",
+        tech: ["React", "JS", "CSS", "Vite"],
+        github: "https://github.com/ValerioDiBiagio/booroad",
+        image: "booroad-home.jpg"
+    },
+    {
+        id: 2,
+        title: "DC Comics",
+        description: "Questo progetto struttura un'interfaccia per navigare e visualizzare una collezione di fumetti DC Comics.",
+        tech: ["React", "JS", "CSS", "Vite"],
+        github: "https://github.com/AlbertoCadorin/react-dc-comics",
+        image: "dc-home.jpg"
+    }, {
+        id: 3,
+        title: "DropBox",
+        description: "Questo progetto consiste nella realizzazione di una landing page ispirata a Dropbox.",
+        tech: ["HTML", "CSS"],
+        github: "https://github.com/AlbertoCadorin/htmlcss-dropbox",
+        image: "dropbox-home.jpg"
+    }, {
+        id: 4,
+        title: "Spotify Web",
+        description: "",
+        tech: ["HTML", "CSS"],
+        github: "https://github.com/AlbertoCadorin/html-css-spotifyweb",
+        image: "spotify-web.jpg"
+    }, {
+        id: 5,
+        title: "Discord Clone",
+        description: "Questo progetto ricrea l'interfaccia e il layout di Discord, focalizzandosi sulla struttura semantica e lo styling moderno.",
+        tech: ["HTML", "CSS"],
+        github: "https://github.com/AlbertoCadorin/html-css-spotifyweb",
+        image: "/discord-home.jpg"
+    }
+]
